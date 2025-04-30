196191d515d2dd0a61ebba3f538ffb99fcc86421
Luís Silva <luis.gustavo.ods@gmail.com>
2025-04-29 21:47:57 -0300

    Initial Commit

---
+++ src/test/java/com/github/projects/hotel_system/HotelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projects.hotel_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
